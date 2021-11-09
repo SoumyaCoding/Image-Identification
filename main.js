659afb17305d1054e731dbaf60d30c5a23c356c3
SoumyaCoding <86145656+SoumyaCoding@users.noreply.github.com>
2021-11-09 14:16:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.attach('camera#');
+camera = document.getElementById('camera');
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality: 90
+});
+
+function takesnapshot()
+{
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML <img id="selfie_image" src="'+data_uri+'"/>
+    });
+}
