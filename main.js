e160ea909aad3f94d9bea05b98ca63bb22b92574
SoumyaCoding <86145656+SoumyaCoding@users.noreply.github.com>
2021-11-10 15:10:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,24 @@ Webcam.set({
 function takesnapshot()
 {
     Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML <img id="selfie_image" src="'+data_uri+'"/>
+    document.getElementById("result").innerHTML <img id ="selfie_image" src="'+data_uri+'"/>
     });
+}
+
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('Model Lodes!');
+}
+
+function check(){
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result){
+if (error) {  
+} else {
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
 }
